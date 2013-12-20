b361a2b3da48e9044ab9e10263a112c8f6c67e26
Ben Dunkle <ben@field2.com>
2013-12-19 20:07:06 -0500

    Changed my last name

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 <head>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
-<title>Ben Smith's blog</title>
+<title>Ben Dunkle's blog...</title>
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
